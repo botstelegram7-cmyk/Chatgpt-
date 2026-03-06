820b052ca6a574395a57edf2213c5f9808d76603
botstelegram7-cmyk <botstelegram7@gmail.com>
2026-03-06 07:58:45 +0530

    Update core.py

--- downloader/core.py
+++ downloader/core.py
@@ -2,12 +2,20 @@ import yt_dlp
 import os
 from config import DL_DIR
 
+os.makedirs(DL_DIR, exist_ok=True)
+
+
 def download(url):
-    os.makedirs(DL_DIR, exist_ok=True)
+
     ydl_opts = {
         "outtmpl": f"{DL_DIR}/%(title)s.%(ext)s",
         "quiet": True
     }
+
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+
         info = ydl.extract_info(url, download=True)
-        return ydl.prepare_filename(info)
+
+        file_path = ydl.prepare_filename(info)
+
+        return file_path
