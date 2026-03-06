001a80b33c6fa066d5874cac2e6d1bb8c5a4fb7c
botstelegram7-cmyk <botstelegram7@gmail.com>
2026-03-06 07:55:44 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -2,7 +2,9 @@ from pyrogram import filters
 from client import app
 from utils.decorators import owner_only
 
-@app.on_message(filters.command("ping") & ~filters.outgoing)
+
+@app.on_message(filters.command("ping"))
 @owner_only
 async def ping(client, message):
-    await message.reply("Pong!")
+
+    await message.reply("🏓 Pong!")
