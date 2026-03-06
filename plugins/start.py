e55a653ca01741b20215bf516a9dee80df5aad7e
botstelegram7-cmyk <botstelegram7@gmail.com>
2026-03-06 07:54:30 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -2,14 +2,23 @@ from pyrogram import filters
 from client import app
 from database import add_user
 
-@app.on_message(filters.command("start") & ~filters.outgoing)
+
+@app.on_message(filters.command("start"))
 async def start_cmd(client, message):
+
     await add_user(message.from_user)
+
     await message.reply_text(
 """⋆｡° ✮ °｡⋆
+
 -ˏˋ⋆ ᴡ ᴇ ʟ ᴄ ᴏ ᴍ ᴇ ⋆ˊˎ-
+
 »»──── ✦ ────««
-Send any video URL to download.
+
+▸ Send any video URL to download
+▸ Supported: YouTube, Instagram, TikTok
+▸ Direct files also supported
+
 ⋆ ｡˚ Serena Downloader Bot ˚｡ ⋆
 """
 )
