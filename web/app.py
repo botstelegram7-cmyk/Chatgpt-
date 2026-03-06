2b5dfc42b650696b532548511b83826e0c46f587
botstelegram7-cmyk <botstelegram7@gmail.com>
2026-03-06 07:56:43 +0530

    Update app.py

--- web/app.py
+++ web/app.py
@@ -4,11 +4,20 @@ from config import PORT
 
 app = Flask(__name__)
 
+
+@app.route("/")
+def home():
+    return "Serena Downloader Bot Running"
+
+
 @app.route("/health")
 def health():
-    return "OK",200
+    return "OK", 200
+
 
 def start_web():
+
     def run():
         app.run(host="0.0.0.0", port=PORT)
-    Thread(target=run).start()
+
+    Thread(target=run, daemon=True).start()
