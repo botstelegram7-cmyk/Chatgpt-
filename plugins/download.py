a2b6eef7d4e6dd349af36e75eba5cfe9a04024e4
botstelegram7-cmyk <botstelegram7@gmail.com>
2026-03-06 07:55:13 +0530

    Update download.py

--- plugins/download.py
+++ plugins/download.py
@@ -2,13 +2,22 @@ from pyrogram import filters
 from client import app
 from downloader.core import download
 
-@app.on_message(~filters.outgoing & filters.text)
+
+@app.on_message(filters.text & ~filters.command(["start","ping"]) & ~filters.outgoing)
 async def handle_url(client, message):
+
     url = message.text.strip()
-    msg = await message.reply("Downloading...")
+
+    msg = await message.reply("⏬ Downloading...")
+
     try:
-        file = download(url)
-        await message.reply_document(file)
+
+        file_path = download(url)
+
+        await message.reply_document(file_path)
+
         await msg.delete()
+
     except Exception as e:
-        await msg.edit(f"Error: {e}")
+
+        await msg.edit(f"❌ Error:\n{e}")
