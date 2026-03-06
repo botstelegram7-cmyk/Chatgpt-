4bc91906c6509abb48410ecc963c184b5b67a7cc
botstelegram7-cmyk <botstelegram7@gmail.com>
2026-03-06 07:57:21 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,8 +4,11 @@ from config import DB_PATH
 
 os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
 
+
 async def init_db():
+
     async with aiosqlite.connect(DB_PATH) as db:
+
         await db.execute("""
         CREATE TABLE IF NOT EXISTS users (
             user_id INTEGER PRIMARY KEY,
@@ -16,12 +19,21 @@ async def init_db():
             joined_at TEXT
         )
         """)
+
         await db.commit()
 
+
 async def add_user(user):
+
     async with aiosqlite.connect(DB_PATH) as db:
+
         await db.execute(
-            "INSERT OR IGNORE INTO users(user_id,username,full_name,joined_at) VALUES(?,?,?,datetime('now'))",
+            """
+            INSERT OR IGNORE INTO users
+            (user_id, username, full_name, joined_at)
+            VALUES (?, ?, ?, datetime('now'))
+            """,
             (user.id, user.username, user.first_name)
         )
+
         await db.commit()
